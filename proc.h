42bf4bf29478bf2a09a804e7d6b594028c03cb4b
Will Roberts <willrob4605@gmail.com>
2026-04-28 13:39:06 -0500

    Add tracemask to process struct

--- proc.h
+++ proc.h
@@ -48,7 +48,9 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
-  char name[16];               // Process name (debugging)
+  char name[16];               // Process name
+  int tracemask;               //trace value
+  (debugging)
 };
 
 // Process memory is laid out contiguously, low addresses first:
