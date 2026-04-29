0f56249bcef0baaef3344d3132c569b9723c4b2a
Will Roberts <willrob4605@gmail.com>
2026-04-29 15:34:23 -0500

    children inherit parents tracemask in fork()

--- proc.c
+++ proc.c
@@ -201,6 +201,7 @@ fork(void)
   np->sz = curproc->sz;
   np->parent = curproc;
   *np->tf = *curproc->tf;
+  np->tracemask = curproc->tracemask;  // child inherits parent's trace mask
 
   // Clear %eax so that fork returns 0 in the child.
   np->tf->eax = 0;
